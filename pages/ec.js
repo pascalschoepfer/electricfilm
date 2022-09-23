a90b34a7cfc726947bff898e6d464a1ad7be55d1
pascalschoepfer <pascal@schoepfer.xyz>
2022-09-23 08:54:11 +0200

    implemented the "ec" site with Alines drawings

--- pages/ec.js
+++ pages/ec.js
@@ -11,7 +11,7 @@ const Ec = () => {
                     <div className="col-xl-2 col-md-3 col-sm-6 col-11 m-0 p-2 container-card" data-bs-toggle="modal"
                          data-bs-target={`#modal${i}`} key={i}>
                         <div className="card">
-                            <Image src={`/aline/aline${i + 10}.jpg`} alt='aline01' priority={true} layout={"fill"}
+                            <Image src={`/aline/aline${i + 10}.jpg`} alt={`Sketch #${i+1}`} priority={true} layout={"fill"}
                                    objectFit={"contain"}/>
                         </div>
                         <div className="modal fade" id={`modal${i}`} tabIndex="-1">
